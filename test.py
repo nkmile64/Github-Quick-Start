4fbcd8e2a34794d581eec7a60243a0a53e54ec03
nkmile64 <nkmile64@gmail.com>
2026-06-26 23:32:24 +0300

    Added labels to the wedges

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
 data = [45, 20, 15, 15, 5]
+labels = ["profit", "rent", "fuel", "SS", "commissions"]
 
 fig, ax = plt.subplots()
-ax.pie(data)
+pie = ax.pie(data)
+ax.pie_label(pie, labels)
 
 plt.show()
